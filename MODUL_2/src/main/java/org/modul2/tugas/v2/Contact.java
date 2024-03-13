dbec417f0543ff3cdc299776b8e88dd77fd5959e
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-03-13 07:54:09 +0700

    Menggunakan generics

--- MODUL_2/src/main/java/org/modul2/tugas/v2/Contact.java
+++ MODUL_2/src/main/java/org/modul2/tugas/v2/Contact.java
@@ -1,14 +1,30 @@
 package org.modul2.tugas.v2;
 
-class Contact {
-    String nama;
-    String nomorTelepon;
-    Contact next;
+class Contact<T> {
+    private T nama;
+    private T nomorTelepon;
+    private Contact<T> next;
 
-    public Contact(String nama, String nomorTelepon) {
+    public Contact(T nama, T nomorTelepon) {
         this.nama = nama;
         this.nomorTelepon = nomorTelepon;
         this.next = null;
+    }
+
+    public T getNama() {
+        return nama;
+    }
+
+    public T getNomorTelepon() {
+        return nomorTelepon;
+    }
+
+    public void setNext(Contact<T> next) {
+        this.next = next;
+    }
+
+    public Contact<T> getNext() {
+        return next;
     }
 
     @Override
--- MODUL_2/src/main/java/org/modul2/tugas/v2/ContactManager.java
+++ MODUL_2/src/main/java/org/modul2/tugas/v2/ContactManager.java
@@ -1,22 +1,22 @@
 package org.modul2.tugas.v2;
 
-class ContactManager {
-    private Contact head;
+class ContactManager<T> {
+    private Contact<T> head;
 
     public ContactManager() {
         head = null;
     }
 
-    public void tambahKontak(String nama, String nomorTelepon) {
-        Contact newKontak = new Contact(nama, nomorTelepon);
+    public void tambahKontak(T nama, T nomorTelepon) {
+        Contact<T> newKontak = new Contact<>(nama, nomorTelepon);
         if (head == null) {
             head = newKontak;
         } else {
-            Contact current = head;
-            while (current.next != null) {
-                current = current.next;
+            Contact<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
             }
-            current.next = newKontak;
+            current.setNext(newKontak);
         }
         System.out.println("Kontak berhasil ditambahkan.");
     }
@@ -27,28 +27,28 @@ class ContactManager {
             return;
         }
         System.out.println("Daftar Kontak:");
-        Contact current = head;
+        Contact<T> current = head;
         while (current != null) {
             System.out.println(current);
-            current = current.next;
+            current = current.getNext();
         }
     }
 
-    public void cariKontak(String nama) {
+    public void cariKontak(T nama) {
         if (head == null) {
             System.out.println("Kontak tidak ditemukan.");
             return;
         }
-        Contact current = head;
+        Contact<T> current = head;
         boolean ditemukan = false;
         while (current != null) {
-            if (current.nama.equalsIgnoreCase(nama)) {
+            if (current.getNama().equals(nama)) {
                 System.out.println("Kontak ditemukan:");
                 System.out.println(current);
                 ditemukan = true;
                 break;
             }
-            current = current.next;
+            current = current.getNext();
         }
         if (!ditemukan) {
             System.out.println("Kontak tidak ditemukan.");
